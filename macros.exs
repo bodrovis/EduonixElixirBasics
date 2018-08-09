c9313677ecad2a2712e5d3166fd2031a48c5acb3
bodrovis <bodrovis@protonmail.com>
2018-08-09 17:16:01 +0300

    10-2

--- macros.exs
+++ macros.exs
@@ -2,8 +2,20 @@ defmodule Demo do
   defmacro work(argument) do
     #argument
     quote do
-      argument * 10
+      unquote(argument) * 10
     end
+  end
+
+  defmacro macro_palindrome?(str, expr) do
+    quote do
+      if( unquote(str) == String.reverse( unquote(str))) do
+        unquote(expr)
+      end
+    end
+  end
+
+  def palindrome?(str, expr) do
+    if str == String.reverse(str), do: expr
   end
 end
 
@@ -11,8 +23,7 @@ defmodule Playground do
   require Demo
 
   def test! do
-    #Demo.work({1,2,3,4,5}) |> elem(2) |> IO.inspect
-    Demo.work(2)|> IO.inspect
+    Demo.macro_palindrome?("123321", IO.puts("found one!"))
   end
 end
 
